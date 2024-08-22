2c7ad9e8beb31f71decc6ed5e54f5a9a9b124345
Achala Punsara <achala.magallagoda@gmail.com>
2024-08-22 08:24:06 +0530

    Protype pattern implementation

---
+++ Creational - Prototype/src/BuildingType.java
@@ -0,0 +1,21 @@
+public class BuildingType {
+
+    private String type;
+
+    public BuildingType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return type;
+    }
+}
